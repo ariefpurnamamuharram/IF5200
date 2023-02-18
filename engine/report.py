ea9e266aae99733435246cc1a4a3a32f5ca6eaaa
Arief Purnama Muharram <23521013@std.stei.itb.ac.id>
2023-02-18 14:26:01 +0700

    Move the report generation function into report.py

--- engine/report.py
+++ engine/report.py
@@ -1,4 +1,11 @@
-def make_report(cardiomegaly: int = 0, effusion: int = 0) -> str:
+import os
+import torch
+
+from engine.inference import Inference
+from engine.utils.image import read_image, get_segment, ToTensorTransform
+
+
+def build_report(cardiomegaly: int = 0, effusion: int = 0) -> str:
     
     try:
         report_text = f'Pada foto radiologi dada yang diterima diperoleh temuan-temuan sebagai berikut: {"Bentuk jantung tampak baik, tidak ditemukan tanda-tanda kardiomegali" if bool(cardiomegaly) else "Terdapat gambaran kardiomegali, CTR > 50%"}. {"Tidak tampak gambaran efusi" if bool(effusion) else "Tampak gambaran efusi"} pada lapang paru.'
@@ -6,3 +13,33 @@ def make_report(cardiomegaly: int = 0, effusion: int = 0) -> str:
         raise ValueError('Report error!')
     
     return report_text
+
+def generate_report(image_path: str, device: torch.device = torch.device('cpu')) -> str:
+    
+    # Read image
+    img = read_image(image_path)
+    img_segment1 = get_segment(img, 1)
+    img_segment2 = get_segment(img, 2)
+    img_segment3 = get_segment(img, 3)
+    
+    # Transform the images into tensors
+    transformer = ToTensorTransform()
+    img_segment1 = transformer.transform(img_segment1)
+    img_segment2 = transformer.transform(img_segment2)
+    img_segment3 = transformer.transform(img_segment3)
+    
+    # Load the models
+    MODELS_DIR = 'enabled/models'
+    model_cardiomegaly = torch.load(os.path.join(MODELS_DIR, 'model_cardiomegaly.pth'))
+    model_effusion = torch.load(os.path.join(MODELS_DIR, 'model_effusion.pth'))
+    
+    # Make inferences
+    inference = Inference(device)
+    result_cardiomegaly = inference.make_prediction(model_cardiomegaly, img_segment2.unsqueeze(0)).item()
+    result_effusion = inference.make_prediction(model_effusion, img_segment2.unsqueeze(0)).item()
+    
+    # Generate the report
+    report = build_report(result_cardiomegaly, result_effusion)
+    
+    # Return the report
+    return report
--- generate_report.py
+++ generate_report.py
@@ -3,8 +3,7 @@ import argparse
 import torch
 
 from engine.inference import Inference
-from engine.report import make_report
-from engine.utils.image import read_image, get_segment, ToTensorTransform
+from engine.report import generate_report
 
 
 if __name__ == '__main__':
@@ -23,31 +22,10 @@ if __name__ == '__main__':
             device = torch.device(args.device)
     else:
         device = torch.device('cpu')
-    
-    # Read image
-    img = read_image(args.image)
-    img_segment1 = get_segment(img, 1)
-    img_segment2 = get_segment(img, 2)
-    img_segment3 = get_segment(img, 3)
-    transformer = ToTensorTransform()
-    img_segment1 = transformer.transform(img_segment1)
-    img_segment2 = transformer.transform(img_segment2)
-    img_segment3 = transformer.transform(img_segment3)
-    
-    # Load the models
-    MODELS_DIR = 'enabled/models'
-    cardiomegaly = torch.load(os.path.join(MODELS_DIR, 'model_cardiomegaly.pth'))
-    effusion = torch.load(os.path.join(MODELS_DIR, 'model_effusion.pth'))
-    
-    # Make inferences
-    inference = Inference(device)
-    result_cardiomegaly = inference.make_prediction(cardiomegaly, img_segment2.unsqueeze(0)).item()
-    result_effusion = inference.make_prediction(effusion, img_segment2.unsqueeze(0)).item()
-    
+        
     # Generate the report
-    report = make_report(result_cardiomegaly, result_effusion)
+    report = generate_report(args.image)
     
     print('\nResults:')
     print(report, '\n')
-    print('Done!')
-       
+    print('Done!')       
